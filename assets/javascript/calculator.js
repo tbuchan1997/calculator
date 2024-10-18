97ae50df9d4a6609d5061ad5a3ec8edd81c9d206
Tom Buchan <tbuchan1997@gmail.com>
2024-10-18 21:06:43 +0000

    Create javascript functions and apply to html inputs

--- assets/javascript/calculator.js
+++ assets/javascript/calculator.js
@@ -1,1 +1,14 @@
-object.onclick = function(){myScript};
+function display(value){
+    document.getElementById("result").value += value;
+}
+
+function clearScreen(){
+    document.getElementById("result").value = "";
+}
+
+function solve(){
+    let x = document.getElementById("result").value;
+    let y = eval(x);
+    document.getElementById("result").value = y;
+    return y;
+}
